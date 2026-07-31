9ce12e64087d264e508dcaf31d9658078e7d6add
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-01 01:10:48 +0530

    Optimized the problem

--- src/Day01/p17.java
+++ src/Day01/p17.java
@@ -3,41 +3,48 @@ package Day01;
 import java.util.Scanner;
 
 public class p17 {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int[] ar=printinput(scan);
-		int M=scan.nextInt();
-		
-		int[] ar1=new int [M];
-		int output=0;
-		for(int i=0;i<M;i++) {
-			ar1[i]=scan.nextInt();
 
-		}
-		
-		for(int i=0;i<M;i++) {
-			output=ar1[i+1]-ar1[i]-1;
-			break;
-		}
-		
-		System.out.println(output);
-		
-	}
-	
-	
-	public static int[] printinput(Scanner scan) {
-		int N=scan.nextInt();
+    public static void main(String[] args) {
 
-		int[] ar=new int[N];
-		
-		for(int i=0;i<N;i++) {
-			ar[i]=i+1;
-		}
-		
-		
-		
-		return ar;
-	}
-	
+        Scanner scan = new Scanner(System.in);
 
+        // Total number of days
+        int N = scan.nextInt();
+
+        // Number of obligation days
+        int M = scan.nextInt();
+
+        // Store obligation days
+        int[] obligation = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            obligation[i] = scan.nextInt();
+        }
+
+        int maxGap = 0;
+
+        // 1. Days before the first obligation
+        maxGap = obligation[0] - 1;
+
+        // 2. Days between consecutive obligations
+        for (int i = 0; i < M - 1; i++) {
+
+            int gap = obligation[i + 1] - obligation[i] - 1;
+
+            if (gap > maxGap) {
+                maxGap = gap;
+            }
+        }
+
+        // 3. Days after the last obligation
+        int lastGap = N - obligation[M - 1];
+
+        if (lastGap > maxGap) {
+            maxGap = lastGap;
+        }
+
+        System.out.println(maxGap);
+
+        scan.close();
+    }
 }
