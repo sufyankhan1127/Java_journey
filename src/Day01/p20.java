a8ed566ded696fa8a6222f0e3b1a963c0dcaf495
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-03 02:54:01 +0530

    print all  armstrong numbers between two inputs

---
+++ src/Day01/p20.java
@@ -0,0 +1,48 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class p20 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int x=scan.nextInt();
+		int y=scan.nextInt();
+		printNumbersbetween(x, y);
+		
+	}
+	
+	static void printNumbersbetween(int x,int y) {
+		
+		for(int i=x+1;i<=y;i++) {
+			int original=i;
+			int count=0;
+			int temp=i;
+			
+			while(temp>0) {
+				count++;
+				temp=temp/10;
+			}
+			
+			temp=i;
+			int sum=0;
+			
+			while(temp>0) {
+				int digit=temp%10;
+				int power=1;
+				for(int j=0;j<count;j++) {
+					power=power*digit;
+				}
+				
+				sum=sum+power;
+				temp=temp/10;
+			}
+			
+			if(sum==original) {
+				System.out.println(original);
+			}
+		}
+
+	}
+
+
+}
