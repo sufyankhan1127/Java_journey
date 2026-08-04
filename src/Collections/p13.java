2bf42e191885ac689f105d27fbb8d4f662d0c9d5
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-05 03:45:18 +0530

    Integer to romans

--- src/Collections/p13.java
+++ src/Collections/p13.java
@@ -1,49 +1,37 @@
 package Collections;
 
-import java.util.Scanner;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class p13 {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter the Integer Value:");
-		String s=scan.nextLine();
-		IntToromans(s);
-	}
-	
-	public static void IntToromans(String s) {
-		Map<Integer,Character> romans=new HashMap<>();
-		
-		romans.put( 1,'I');
-		romans.put(5,'V');
-		romans.put(10,'X');
-		romans.put(50,'L');
-		romans.put(100,'C');
-		romans.put(500,'D');
-		romans.put(1000,'M');
-		
-		String sum="";
-		for(int i=0;i<s.length();i++) {
-			int num=s.charAt(i)-'0';
-			
-			Character current=romans.get(s.charAt(i));
-			
-			if(i<s.length()-1) {
-				Character next=romans.get(s.charAt(i+1));
-				if(current<next) {
-					sum=
-				}
-				
-			}
-			else {
-				sum=sum+current;
-			}
-		}
-		
-		
-		System.out.println("The Roman value for this is : "+sum);
-		
-	}
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] values = {
+                1000,900,500,400,
+                100,90,50,40,
+                10,9,5,4,1
+        };
+
+        String[] romans = {
+                "M","CM","D","CD",
+                "C","XC","L","XL",
+                "X","IX","V","IV","I"
+        };
+
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+
+            while(num >= values[i]){
+                ans.append(romans[i]);
+                num -= values[i];
+            }
+
+        }
+
+        System.out.println(ans);
+    }
 }
