3cf4d899b54f448d20b1b78879f71b23fc4efef5
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-07 03:26:10 +0530

    A faster approach for int to romans

--- src/Day01/p32.java
+++ src/Day01/p32.java
@@ -8,12 +8,17 @@ public class p32 {
 
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        
+        inttoromans1(num);
+    }
 
-        int[] values = {
+        
+    public static void intToRomans(int num) 
+    {
+    	int[] values = {
                 1000,900,500,400,
                 100,90,50,40,
-                10,9,5,4,1
-        };
+                10,9,5,4,1 };
 
         String[] romans = {
                 "M","CM","D","CD",
@@ -34,4 +39,35 @@ public class p32 {
 
         System.out.println(ans);
     }
+    
+    
+    public static void inttoromans1(int num) {
+    	String[] thousands = {
+    		    "", "M", "MM", "MMM"
+    		};
+
+    		String[] hundreds = {
+    		    "", "C", "CC", "CCC", "CD",
+    		    "D", "DC", "DCC", "DCCC", "CM"
+    		};
+
+    		String[] tens = {
+    		    "", "X", "XX", "XXX", "XL",
+    		    "L", "LX", "LXX", "LXXX", "XC"
+    		};
+
+    		String[] ones = {
+    		    "", "I", "II", "III", "IV",
+    		    "V", "VI", "VII", "VIII", "IX"
+    		};
+    		
+    		String ans =
+    		        thousands[num / 1000] +
+    		        hundreds[(num % 1000) / 100] +
+    		        tens[(num % 100) / 10] +
+    		        ones[num % 10];
+    		
+    		System.out.println(ans);
+    }
+    
 }
