4baf150d021b19918afc342dba898c0bf19b52b0
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-07 02:30:28 +0530

    switch case for roman to int

--- src/Day01/p31.java
+++ src/Day01/p31.java
@@ -7,7 +7,7 @@ public class p31 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		String s=scan.nextLine();
-		romantoint(s);
+		romantoint1(s);
 		
 	}
 	
@@ -42,6 +42,48 @@ public class p31 {
 		}
 //		sum=sum+map.get(s.charAt(s.length()-1));
 		
+		System.out.println(sum);
+	}
+	
+	public static int getValue(char ch) {
+		switch (ch) {
+		case 'I': 
+			return 1;
+		case 'V':
+			return 5;
+		case 'L':
+			return 50;
+		case 'X':
+			return 10;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return 0;
+		}
+	}
+	
+	public static void romantoint1(String s) {
+		int sum=0;
+		for(int i=0;i<s.length();i++) {
+			int current=getValue(s.charAt(i));
+			
+			if(i==s.length()-1) {
+				sum=sum+current;
+			}
+			else {
+				int next=getValue(s.charAt(i+1));
+				if(current<next) {
+					sum=sum-current;
+				}
+				else {
+					sum=sum+current;
+				}
+			}
+		}
 		System.out.println(sum);
 	}
 
