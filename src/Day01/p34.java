13482a691beddb24be30b2c403afd0477ca0319c
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-08 16:46:54 +0530

    Min pair sum

---
+++ src/Day01/p34.java
@@ -0,0 +1,40 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class p34 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter the size of the array :");
+		
+		int n=scan.nextInt();
+		int[] ar=new int[n];
+		for(int i=0;i<n;i++) {
+			ar[i]=scan.nextInt();
+		}
+		
+
+		System.out.println(minPairSum(ar));
+		
+	}
+	
+	public static int minPairSum(int[] ar) {
+		int smallest=Integer.MAX_VALUE;
+		int secondsmallest=Integer.MAX_VALUE;
+		
+		for(int i=0;i<ar.length;i++) {
+			if(ar[i]<smallest) {
+				secondsmallest=smallest;
+				smallest=ar[i];
+			}
+			
+			if(ar[i]<secondsmallest && ar[i]>smallest) {
+				secondsmallest=ar[i];
+			}
+		}
+		
+		return smallest+secondsmallest;
+	}
+
+}
