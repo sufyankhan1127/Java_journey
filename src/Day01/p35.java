dcf51cec21fc061ec27acc5f0111d408b1963835
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-09 18:03:30 +0530

    find the character in a string

--- src/Day01/p35.java
+++ src/Day01/p35.java
@@ -1,5 +1,29 @@
+//check whether the given character is present in a string
+
 package Day01;
 
+import java.util.Scanner;
+
 public class p35 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		char ch=scan.next().charAt(0);
+		checPresence(s, ch);
+	}
+	
+	public static void checPresence(String s,char ch) {
+		boolean a=false;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)==ch) {
+				a=true;
+				break;
+			}
+		}
+		
+		if(a==true) {
+			System.out.println("Found");
+		}
+	}
 
 }
