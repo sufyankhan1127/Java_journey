9d832b8e62a6b8ecf6535bda3da166a5f7b9836b
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-11 16:15:38 +0530

    Leetcode 150 second  problem 88

---
+++ src/leetcode150/l2.java
@@ -0,0 +1,46 @@
+//merge two sorted arrays
+
+package leetcode150;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class l2 {
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int len1=scan.nextInt();
+		int nums1[]=new int[len1];
+		
+		for(int i=0;i<len1;i++) {
+			nums1[i]=scan.nextInt();
+		}
+		
+		int len2=scan.nextInt();
+		int nums2[]=new int[len1];
+		
+		for(int i=0;i<len2;i++) {
+			nums2[i]=scan.nextInt();
+		}
+		
+		int m=scan.nextInt();
+		int n=scan.nextInt();
+		
+		merge(nums1,m,nums2,n);
+		
+	}
+
+
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+		for(int i=0;i<n;i++){
+			nums1[m+i]=nums2[i];
+		}
+		Arrays.sort(nums1);
+		for(int l=0;l<nums1.length;l++){
+			System.out.print(nums1[l]+",");
+		}
+	}
+
+
+}
