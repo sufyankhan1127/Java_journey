c119efda48438f2276145dc1b482c041f8a01aec
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-11 16:25:05 +0530

    Leetcode 88 optimized solution

--- src/leetcode150/l2.java
+++ src/leetcode150/l2.java
@@ -31,16 +31,45 @@ public class l2 {
 	}
 
 
+//	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//
+//		for(int i=0;i<n;i++){
+//			nums1[m+i]=nums2[i];
+//		}
+//		Arrays.sort(nums1);
+//		for(int l=0;l<nums1.length;l++){
+//			System.out.print(nums1[l]+",");
+//		}
+//	}
+	
+	
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
-		for(int i=0;i<n;i++){
-			nums1[m+i]=nums2[i];
-		}
-		Arrays.sort(nums1);
-		for(int l=0;l<nums1.length;l++){
-			System.out.print(nums1[l]+",");
-		}
-	}
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } 
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        // What happens if nums2 still has elements?
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
 
 
 }
