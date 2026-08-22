66acae3bafb0ad1df1c8dbc576b21d51e999f0e4
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-22 20:22:23 +0530

    Updated code with best test cases

--- src/leetcode150/l11.java
+++ src/leetcode150/l11.java
@@ -2,4 +2,55 @@ package leetcode150;
 
 public class l11 {
 
+	public int hIndex(int[] citations) {
+
+		for(int i=0;i<citations.length;i++){
+			for(int j=0;j<citations.length-1-i;j++){
+				if(citations[j]>citations[j+1]){
+					int temp=citations[j];
+					citations[j]=citations[j+1];
+					citations[j+1]=temp;
+				}
+			}
+		}
+
+//		int res=0;
+//
+//		if((citations.length%2)!=0){
+//			res=citations[(citations.length/2)];
+//
+//		}
+//		else if(citations.length<2){
+//			return citations[0];
+//		}
+//
+//		else{
+//			res=citations[(citations.length/2)-1];
+//
+//		}
+//
+//
+//		return res;
+		
+		
+		int res = 0;
+
+        for (int i = citations.length - 1; i >= 0; i--) {
+
+            int papers = citations.length - i;
+
+            if (citations[i] >= papers) {
+                res = papers;
+            } else {
+             break;
+            }
+        }
+
+        return res;
+        
+
+	}
+
+
 }
+
