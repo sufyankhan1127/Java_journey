e8ced406e0cf53a31ce74e01e628db1cdc7f9cf0
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-09-05 04:16:34 +0530

    finding the value in linked list

--- src/linkedlists/linkedlist3.java
+++ src/linkedlists/linkedlist3.java
@@ -38,15 +38,23 @@ public class linkedlist3 {
 		
 		System.out.println("Enter the value to find");
 		int findvalue=scan.nextInt();
+		boolean found=false;
 		Node temp=head;
 		while(temp!=null) {
 			if(temp.data==findvalue) {
-				System.out.println("Found the value");
+				found=true;
 				break;
 			}
 			
-			System.out.println("Value not found");
 			temp=temp.next;
+			
+		}
+		
+		if(found) {
+			System.out.println("Found the value");
+		}
+		else {
+			System.out.println("Value not found");
 		}
 		
 	}
