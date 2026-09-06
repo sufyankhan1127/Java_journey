c134aeb4755f9919a3d39eecf74a17969c61d143
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-09-06 14:59:06 +0530

    Treemap problem 1

--- src/treemap/tm1.java
+++ src/treemap/tm1.java
@@ -1,5 +1,37 @@
+//frequency counting and sorted order
+
 package treemap;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class tm1 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the length of array");
+		int n=scan.nextInt();
+		int[] ar=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			
+			System.out.println("Enter the value of "+(i+1));
+			ar[i]=scan.nextInt();
+		}
+		
+		findOccandSort(ar);
+	}
+	
+	public static void findOccandSort(int[] ar) {
+		TreeMap<Integer, Integer> map=new TreeMap();
+		
+		for(int x:ar) {
+			map.put(x, map.getOrDefault(x, 0)+1);
+		}
+		
+		for(Map.Entry<Integer, Integer> x:map.entrySet()) {
+			System.out.println(x.getKey()+" -> "+x.getValue());
+		}
+	}
 
 }
