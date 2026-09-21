848418c6b00f89eb4149a94cc5b8ddfd8b4f1438
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-09-21 15:19:04 +0530

    Learning . and :: difference

--- src/streamsAPI/p1.java
+++ src/streamsAPI/p1.java
@@ -8,10 +8,27 @@ import java.util.Scanner;
 public class p1 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		List <Integer> list=Arrays.asList(10,20,30,9,56,22,3,5,25);
+		List <Integer> list=Arrays.asList(10,20,30,9,56,22,3,5,25);//This works
+		/*ArrayList <Integer> list=Arrays.asArrayList(10,20,30,9,56,22,3,5,25);// This doesnt*/
+		
+		//IF we specifically want an arrayList we need to declare it like this
+		ArrayList<Integer> list1=new ArrayList<>(Arrays.asList(10,8,7,12,45,2,15,10));
 		System.out.println("Enter the value to filter less than elements");
 		int input=scan.nextInt();
-		list.stream().filter(n->n>input).forEach(System.out::println);
+		list.stream()
+		.filter(n->n>input)
+		.forEach(System.out::println);//why ::println but not .println what is the difference between both
+		/*This is Java 8 method references meaning use println() method call whenever needed*/
+		
+//		. means:
+//
+//			Access/call something on an object.
+		
+		
+		
+//		:: means:
+//
+//			"Use this existing method as a function."
 	}
 
 }
